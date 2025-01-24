729da1ff3603dc3a81a51be172702999be4e76c3
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-01-24 12:01:22 +0100

    feat: add CI pipeline, docs, and packaging setup
    Introduce a GitHub Actions CI pipeline for unit tests, linting, coverage, and multi-environment testing. Add a Sphinx-based documentation setup and publish configuration for ReadTheDocs. Include packaging tools, pre-commit hooks, and scripts for PyPI publishing.

---
+++ .ambient-package-update/metadata.py
@@ -0,0 +1,47 @@
+from ambient_package_update.metadata.author import PackageAuthor
+from ambient_package_update.metadata.constants import (
+    DEV_DEPENDENCIES,
+    LICENSE_MIT,
+    SUPPORTED_DJANGO_VERSIONS,
+)
+from ambient_package_update.metadata.maintainer import PackageMaintainer
+from ambient_package_update.metadata.package import PackageMetadata
+from ambient_package_update.metadata.readme import ReadmeContent
+
+METADATA = PackageMetadata(
+    module_name="pwa_courier",
+    package_name="django-pwa-courier",
+    github_package_group="christoph-teichmeister",
+    authors=[
+        PackageAuthor(
+            name="Chris Teichmeister",
+            email="christoph.teichmeister@gmail.com",
+        ),
+    ],
+    maintainer=PackageMaintainer(
+        name="Chris Teichmeister",
+        url="https://chris.teichmeister.lu/",
+        email="christoph.teichmeister@gmail.com",
+    ),
+    company="-",
+    license=LICENSE_MIT,
+    license_year=2025,
+    development_status="4 - Beta",
+    has_migrations=False,
+    readme_content=ReadmeContent(uses_internationalisation=False),
+    dependencies=[
+        f"Django>={SUPPORTED_DJANGO_VERSIONS[0]}",
+        "pywebpush>=2",
+    ],
+    supported_django_versions=SUPPORTED_DJANGO_VERSIONS,
+    supported_python_versions=[
+        "3.10",
+        "3.11",
+        "3.12",
+        "3.13",
+    ],
+    optional_dependencies={
+        "dev": [*DEV_DEPENDENCIES],
+    },
+    ruff_ignore_list=[],
+)
---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+# Changelog
+
+// TODO CT: Local installation with `pip install -e <path>`
+
+* **1.0.0** (2025-01-24)
+  * Release as a package at PyPI
