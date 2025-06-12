5b3986a83558f1628976852bc74ffd1d8b206534
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-06-12 21:34:37 +0200

    feat: introduce app configuration for django_pwa_courier
    Added initial app configuration class for the `django_pwa_courier` package to support Django application registry.
    
    - Created `DjangoPWACourierAppConfig` in `apps.py`.
    - Defined app name and verbose name for clarity.

---
+++ django_pwa_courier/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class DjangoPWACourierAppConfig(AppConfig):
+    name = "django_pwa_courier"
+    verbose_name = "django-pwa-courier"
