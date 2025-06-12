469f4ee80aa4f9ef0129bb103cdea1afd0fb6642
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-06-12 21:22:48 +0200

    feat: add URLs for PWA views
    Introduced URL patterns for PWA-related views to enhance routing and functionality.
    
    - Added `manifest.json` endpoint for serving the PWA manifest.
    - Added `offline/` endpoint for the offline page view.
    - Added `handle-protocol/` endpoint for handling PWA protocols.
    - Added `serviceworker.js` endpoint for serving the service worker.

---
+++ django_pwa_courier/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from django_pwa_courier import views
+
+app_name = "django_pwa_courier"
+
+urlpatterns = [
+    path("manifest.json", views.ManifestView.as_view(), name="manifest"),
+    path("offline/", views.OfflineView.as_view(), name="offline"),
+    path("handle-protocol/", views.HandleProtocolView.as_view(), name="handle_protocol"),
+    path("serviceworker.js", views.ServiceWorkerView.as_view(), name="serviceworker"),
+]
