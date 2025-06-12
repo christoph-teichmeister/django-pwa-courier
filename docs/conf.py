8880413747559ce03073feb7a0d8d25b0cc77c71
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-06-12 21:19:26 +0200

    refactor: update PWA package references in docs
    Replaced "pwa_courier" with "django_pwa_courier" in the documentation configuration to reflect the updated package name.
    
    - Updated `docs/conf.py` import statements and app references.
    - Ensured consistency with recent renaming changes in the codebase.

--- docs/conf.py
+++ docs/conf.py
@@ -25,13 +25,13 @@ settings.configure(
         "django.contrib.sessions",
         "django.contrib.messages",
         "django.contrib.staticfiles",
-        "pwa_courier",
+        "django_pwa_courier",
     ],
     SECRET_KEY="ASDFjklö123456890",
 )
 django.setup()
 
-from pwa_courier import __version__  # noqa: E402
+from django_pwa_courier import __version__  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
