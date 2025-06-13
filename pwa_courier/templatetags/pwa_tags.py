93d3bb67a52eef36f64996516174a78c37092f77
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-06-13 08:45:22 +0200

    refactor: use getter methods for PWA settings
    Replaced direct accesses to PWA settings with getter methods for improved modularity and encapsulation.
    
    - Updated `load_pwa_meta_data` to use `get_pwa_manifest` method.
    - Updated `load_serviceworker` to use `get_pwa_manifest`, `get_pwa_service_worker_debug`, and `get_pwa_webpush_settings`.

--- pwa_courier/templatetags/pwa_tags.py
+++ pwa_courier/templatetags/pwa_tags.py
@@ -7,13 +7,13 @@ register = template.Library()
 
 @register.inclusion_tag("pwa_courier/pwa/load_meta_data.html", takes_context=True)
 def load_pwa_meta_data(context):
-    return {"PWA_MANIFEST": settings.PWA_MANIFEST}
+    return {"PWA_MANIFEST": settings.get_pwa_manifest()}
 
 
 @register.inclusion_tag("pwa_courier/pwa/load_serviceworker.html", takes_context=True)
 def load_serviceworker(context):
     return {
-        "PWA_MANIFEST": settings.PWA_MANIFEST,
-        "PWA_SERVICE_WORKER_DEBUG": settings.PWA_SERVICE_WORKER_DEBUG,
-        "PWA_WEBPUSH_SETTINGS": settings.PWA_WEBPUSH_SETTINGS,
+        "PWA_MANIFEST": settings.get_pwa_manifest(),
+        "PWA_SERVICE_WORKER_DEBUG": settings.get_pwa_service_worker_debug(),
+        "PWA_WEBPUSH_SETTINGS": settings.get_pwa_webpush_settings(),
     }
--- tests/templatetags/test_pwa_tags.py
+++ tests/templatetags/test_pwa_tags.py
@@ -6,14 +6,14 @@ from pwa_courier.templatetags.pwa_tags import load_pwa_meta_data, load_servicewo
 
 class PWATagsTest(TestCase):
     def test_load_pwa_meta_data_returns_pwa_manifest_dict(self):
-        self.assertEqual(load_pwa_meta_data(context=None), {"PWA_MANIFEST": settings.PWA_MANIFEST})
+        self.assertEqual(load_pwa_meta_data(context=None), {"PWA_MANIFEST": settings.get_pwa_manifest()})
 
     def test_load_serviceworker_returns_relevant_dict(self):
         self.assertEqual(
             load_serviceworker(context=None),
             {
-                "PWA_MANIFEST": settings.PWA_MANIFEST,
-                "PWA_SERVICE_WORKER_DEBUG": settings.PWA_SERVICE_WORKER_DEBUG,
-                "PWA_WEBPUSH_SETTINGS": settings.PWA_WEBPUSH_SETTINGS,
+                "PWA_MANIFEST": settings.get_pwa_manifest(),
+                "PWA_SERVICE_WORKER_DEBUG": settings.get_pwa_service_worker_debug(),
+                "PWA_WEBPUSH_SETTINGS": settings.get_pwa_webpush_settings(),
             },
         )
